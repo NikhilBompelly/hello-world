45d8ab953dd83aa85dc5ea721168e684065b8c7a
NikhilBompelly <30628680+NikhilBompelly@users.noreply.github.com>
2018-08-21 14:54:11 +0530

    9pavone
    created a 9pavone website by using the known technologies

---
+++ 9pavone/js/main.js
@@ -0,0 +1,35 @@
+// $(document).ready(function(){
+//  $(".nav-item a").click(function(){
+// // $(".dropdown-content").hover(function(){
+//    $(".dropdown-content").slideDown();
+// });
+// });
+
+  //   $(".dropdown-content").slideUp();
+  // if($(this).next().is(":visible")){
+  //
+  //   $(this).next().slideUp();
+  //   }
+  //   else {
+  //     $(this).next().slideToggle();
+  //   }
+ //
+ // alert("this is");
+  //
+  // $('.dropdown-content').slideUp();
+
+  // 			if ($(this).next('.dropdown-content').is(':visible')) {
+  //
+  // 				$(this).next('.dropdown-content').slideUp();
+  // 			} else {
+  //
+  // 				$(this).next('.dropdown-content').slideToggle();
+  // 			}
+
+  // $(this).next().slideToggle();
+
+
+  $(document).ready(function(){
+
+    alert("nikhil");
+  });
